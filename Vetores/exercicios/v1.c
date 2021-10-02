99b7ebac047b6be8f7563a2dc4aa3fa244b86831
Yuri Soares <91438344+yurisoaresm@users.noreply.github.com>
2021-10-02 11:13:51 -0300

    Update v1.c

--- Vetores/exercicios/v1.c
+++ Vetores/exercicios/v1.c
@@ -3,7 +3,7 @@
 #define T 3
 int main(void) {
 	SetConsoleOutputCP(65001);
- 	int vetor[T], i;
+ 	int vetor[T], i, somaMult = 0, quantidade = 0;
 	printf("Digite 10 números inteiros positivos.\n");
 	for (i = 0; i < T; i++){
 		printf("%i°: ", i +1);
@@ -25,5 +25,22 @@ int main(void) {
 		if (vetor[i] % 2 != 0 && vetor[i] % 7 == 0)
 			printf("%i (índice %i) ", vetor[i], i);
 	}
+	// Soma dos múltiplos de 3 e 4
+	printf("\n\nSoma dos múltiplos de 3 e 4: ");
+	for (i = 0; i < T; i++){
+		if (vetor[i] % 3 == 0 && vetor[i] % 4 == 0)
+			somaMult += vetor[i];
+	}
+	printf("%i ", somaMult);
+	// Média da soma dos termos múltiplos de 2 e 5
+	somaMult = 0;
+	printf("\n\nMédia da soma dos múltiplos de 2 e 5: ");
+	for (i = 0; i < T; i++){
+		if (vetor[i] % 2 == 0 && vetor[i] % 5 == 0){
+			somaMult += vetor[i];
+			quantidade++;
+		}
+	}
+	printf("%i ", somaMult/quantidade);
   return 0;
 }
