84e25418ac6a62987f7ecb18f79ed7d9d50d7517
Yuri Soares <91438344+yurisoaresm@users.noreply.github.com>
2021-10-02 16:45:18 -0300

    Update v1.c

--- Vetores/exercicios/v1.c
+++ Vetores/exercicios/v1.c
@@ -4,6 +4,7 @@
 int main(void) {
 	SetConsoleOutputCP(65001);
  	int vetor[T], i, somaMult = 0, quantidade = 0;
+
 	printf("Digite 10 números inteiros positivos.\n");
 	for (i = 0; i < T; i++){
 		printf("%i°: ", i +1);
@@ -13,18 +14,21 @@ int main(void) {
 	for (i = 0; i < T; i++){
 		printf("%i ", vetor[i]);
 	}
+
 	// Múltiplos de 3
 	printf("\n\nMúltiplos de 3: ");
 	for (i = 0; i < T; i++){
 		if (vetor[i] % 3 == 0)
 			printf("%i (índice %i) ", vetor[i], i);
 	}
+
 	// Ímpares e múltiplos de 7
 	printf("\n\nÍmpares e múltiplos de 7: ");
 	for (i = 0; i < T; i++){
 		if (vetor[i] % 2 != 0 && vetor[i] % 7 == 0)
 			printf("%i (índice %i) ", vetor[i], i);
 	}
+
 	// Soma dos múltiplos de 3 e 4
 	printf("\n\nSoma dos múltiplos de 3 e 4: ");
 	for (i = 0; i < T; i++){
@@ -32,6 +36,7 @@ int main(void) {
 			somaMult += vetor[i];
 	}
 	printf("%i ", somaMult);
+	
 	// Média da soma dos termos múltiplos de 2 e 5
 	somaMult = 0;
 	printf("\n\nMédia da soma dos múltiplos de 2 e 5: ");
@@ -42,5 +47,6 @@ int main(void) {
 		}
 	}
 	printf("%i ", somaMult/quantidade);
+	
   return 0;
 }
