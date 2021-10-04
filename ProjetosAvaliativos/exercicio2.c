5fa5d80add3d64e66e73607134bae3e8e28c1f68
Yuri Soares <91438344+yurisoaresm@users.noreply.github.com>
2021-10-04 14:02:14 -0300

    Create exercicio2.c

---
+++ ProjetosAvaliativos/exercicio2.c
@@ -0,0 +1,28 @@
+// Exibir na tela os termos (e seus respectivos índices) ÍMPARES e múltiplos de 7.
+
+
+#include<stdio.h>
+#include <Windows.h>
+
+#define SIZE 3
+int main(int argc, char const *argv[]){
+    SetConsoleOutputCP(65001);
+    int vetor[SIZE];
+    int i;
+    
+    for(i = 0; i < SIZE; i++){
+        printf("Digite %iº número: ", i + 1);
+        scanf("%i", &vetor[i]);
+        while (vetor[i] < 0){
+            printf("ERRO: o número deve ser positivo. Digite novamente: ");
+            scanf("%i", &vetor[i]);
+        }
+    }
+
+    printf("\nOs números ímpares e múltiplos de 7 armazenados foram:\n");
+    for(i = 0; i < SIZE; i++){
+        if (vetor[i] % 2 != 0 &&  vetor[i] % 7 == 0)
+            printf("%i (índice %i)\n", vetor[i], i);
+    }
+    return 0;
+}
