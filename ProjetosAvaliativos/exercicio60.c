ac9c5d6cd5b8a5da245476ead70c60b67469f8c0
Yuri Soares <91438344+yurisoaresm@users.noreply.github.com>
2021-10-05 11:54:51 -0300

    Create exercicio60.c

---
+++ ProjetosAvaliativos/exercicio60.c
@@ -0,0 +1,12 @@
+// Faça um programa que preencha uma Matriz M (3x2) com valores inteiros aleatórios: 0 ou 1. Crie e exiba na tela a Matriz Trasposta MT (2x3) da Matriz preenchida aleatoriamente.
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <Windows.h>
+
+int main(int argc, char const *argv[]){
+    
+    
+    return 0;
+}
