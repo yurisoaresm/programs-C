2455b88113142f4e46ae6b241c8df92c0139dc96
Yuri Soares <91438344+yurisoaresm@users.noreply.github.com>
2021-10-05 07:29:20 -0300

    Update exercicio35.c

--- ProjetosAvaliativos/exercicio35.c
+++ ProjetosAvaliativos/exercicio35.c
@@ -8,17 +8,17 @@
 int main(int argc, char const *argv[]){
     SetConsoleOutputCP(65001);
     float tempoCorrida[SIZE];
-    int matricula[SIZE];
-    int i;
+    int matricula[SIZE], i;
 
-    for (i = 0; i < matricula[SIZE]; i++){
+    for (i = 0; i < SIZE; i++){
         printf("Digite o número da matrícula do %iº atleta: ", i + 1);
         scanf("%i", &matricula[i]);
         printf("Digite agora o tempo de corrida do %iº atleta (em minutos): ", i + 1);
         scanf("%f", &tempoCorrida[i]);
     }
+
     printf("\nAtletas que ficaram ABAIXO da meta (88.6 min) da prova: ");
-    for (i = 0; i < tempoCorrida[SIZE]; i++){
+    for (i = 0; i < SIZE; i++){
         if (tempoCorrida[i] < META)
             printf("\nAtleta: %i", matricula[i]);
     }
