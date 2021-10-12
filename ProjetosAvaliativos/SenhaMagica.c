9efe1d126266f620efff3bc1881c9a318fae21e8
Yuri Soares <91438344+yurisoaresm@users.noreply.github.com>
2021-10-12 15:37:33 -0300

    Update indentation

--- ProjetosAvaliativos/SenhaMagica.c
+++ ProjetosAvaliativos/SenhaMagica.c
@@ -11,27 +11,26 @@ Descobrir a senha da porta mágica, de 1 a 100. Com isso:
 #include <time.h>
 #include <stdlib.h>
 int main(void) {
-  int Segredo, opcao, chances, chute;
-  srand (time(NULL));
-  do{
-    Segredo = rand() % 100 + 1;
-    printf("MENU DO JOGO: \n");
-    printf("Escolha: 1 (Jogar) ou 0 (Sair): ");
-    printf("Sua opção: ");
-    scanf("%i", &opcao);
-    if (opcao == 0)
-      printf("\nFIM DO JOGO. \n");
-    else if (opcao != 1)
-      printf("Erro de opção. Escolha de novo.\n\n");
-    else{
+	int Segredo, opcao, chances, chute;
+	srand (time(NULL));
+	do{
+		Segredo = rand() % 100 + 1;
+		printf("MENU DO JOGO: \n");
+		printf("Escolha: 1 (Jogar) ou 0 (Sair): ");
+		printf("Sua opção: ");
+		scanf("%i", &opcao);
+		if (opcao == 0)
+			printf("\nFIM DO JOGO. \n");
+		else if (opcao != 1)
+			printf("Erro de opção. Escolha de novo.\n\n");
+		else{
 			chances = 0;
-      while (chances < 7){
+			while (chances < 7){
 				printf("%i° chance: escolha um número de 1 e 100: ", chances + 1);
 				scanf("%i", &chute);
-			if (chute <1 || chute > 100)
-				printf("Eu disse um número de 1 a 100.\n\n");
-			else
-				if (chute == Segredo){
+				if (chute < 1 || chute > 100)
+					printf("Eu disse um número de 1 a 100.\n\n");
+				else if (chute == Segredo){
 					printf("Você acertou a Senha da Porta Mágica. Parabéns!\n\n");
 					chances = 8;
 				} else{
@@ -41,15 +40,14 @@ int main(void) {
 					} else{
 						printf("Esse número é MAIOR que o Segredo da Porta.\n\n");
 						chances++;
-						}
 					}
 				}
-				if (chances == 7){
-					printf("Suas chances acabaram. Morra, infeliz!\n");
-					printf("A Senha era %i \n\n", Segredo);
-		
-				} 
 			}
-  }while(opcao != 0);
-  return 0;
+			if (chances == 7){
+				printf("Suas chances acabaram. Morra, infeliz!\n");
+				printf("A Senha era %i \n\n", Segredo);
+			} 
+		}
+	} while(opcao != 0);
+	return 0;
 }
