2aafda78bcfa3a0a6616a114d1167ef24cee7ac6
Yuri Soares <91438344+yurisoaresm@users.noreply.github.com>
2021-10-20 11:10:47 -0300

    Update ArvoreNatal.c

--- Desafios/ArvoreNatal.c
+++ Desafios/ArvoreNatal.c
@@ -1,18 +1,27 @@
 // árvore de natal
-
+// Condição: N é impar e 1 < N < 100
+#include <stdio.h>
+#include <stdlib.h>
 int main(int argc, char const *argv[]){
-  int N, i;
+  int N, i = 1, spaces = 1;
+  char string[N];
 
   do{
     printf("Digite um inteiro ímpar e entre 1 e 100: ");
     scanf("%i", &N);
     printf("\n");
-  } while (N <= 2 || N >= 100 && N % 2 == 0);
+  } while (N <= 2 || N >= 100 || N % 2 == 0);
 
-  for (i = 1; i <= N; i+2){
-    
+  for (i = 1; i <= N; i += 2){
+    if (i == 1){
+      while (spaces <= (N + 1)/2 - i){
+        printf(" ");
+        spaces++;
+      }
+    }
+    printf("*");
+    spaces = 1;
   }
-  printf("*");
 
   return 0;
 }
