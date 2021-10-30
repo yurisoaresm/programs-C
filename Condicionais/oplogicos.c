8ef53f58c2388fb6818938f638b08a20f70bf012
Yuri Soares <91438344+yurisoaresm@users.noreply.github.com>
2021-10-30 19:44:10 -0300

    Create oplogicos.c

---
+++ Condicionais/oplogicos.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/* 
+ * Conjunção (E / AND) -> &&
+ * 0 0 - 0 false
+ * 1 0 - 0 false
+ * 0 1 - 0 false
+ * 1 1 - 1 true
+ * 
+ * Disjunção (OU / OR) -> ||
+ * 0 0 - 0 false
+ * 1 0 - 1 true
+ * 0 1 - 1 true
+ * 1 1 - 1 true
+ */ 
+int main(void){
+  int a = 5, b = 5, c = 8;
+
+  printf("%d\n", a == b);
+  printf("%d\n", a == c);
+  printf("%d\n", a == b && a == c);
+  printf("%d\n", a == b || a == c);
+  printf("%d\n", !(a == b));
+  printf("%d\n", !(a == b) && a == 10 || c == 8);
+  printf("%d\n", !(32 <= a) && a*b == 5*5);
+  return 0;
+}
