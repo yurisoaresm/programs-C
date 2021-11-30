5190d773c3525f7d925bf907fed369d4eb265420
Yuri Soares <91438344+yurisoaresm@users.noreply.github.com>
2021-11-29 21:23:57 -0300

    3 exercises commit
    Exercícios 7, 8 e 15 de modularização.

---
+++ ProjetosAvaliativos/mod_ex7.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int anoBissexto(int ano);
+
+int anoBissexto(int ano){
+  if (ano % 400 == 0 || (ano % 4 == 0 && ano % 100 != 0))
+    return 1;
+  else
+    return 0;
+}
+
+int main(void) {
+  int ano, count = 1;
+
+  while (count <= 100){
+    printf("Digite um ano para e veja se é bissexto: ");
+    scanf("%i", &ano);
+    if (ano < 0)
+      printf("ERRO: o ano não pode ser negativo.\n");
+    else{
+      printf("O ano %i é: %i\n", ano, anoBissexto(ano));
+      count++;
+    }
+
+  }
+  
+  return 0;
+}
---
+++ ProjetosAvaliativos/mod_ex8.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <math.h>
+#define pi 3.14
+
+float logaritmo(int N);
+
+float logaritmo(int N){
+  int i, divisor;
+  
+  for (i = 1; i <= N; i++){
+    if (N % i == 0)
+      divisor++;
+  }
+  
+  if (divisor > 2) // N não é primo
+    return log2(N)/log2(pi);
+  else 
+    return log2(pi)/log2(N);
+}
+
+int main(void) {
+  int N, count = 1;
+
+  while (count <= 500){
+    printf("Digite N: ");
+    scanf("%i", &N);
+
+    if (N < 0)
+      printf("ERRO: N não pode ser negativo.\n");
+    else{
+      printf("O cálculo é: %.2f\n", logaritmo(N));
+      count++;
+    }
+  }
+  return 0;
+}
