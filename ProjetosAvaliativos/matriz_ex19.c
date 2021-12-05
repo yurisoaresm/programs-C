1a409e087c641f42b7b4ffe396183589eba3cf83
Yuri Soares <91438344+yurisoaresm@users.noreply.github.com>
2021-12-05 19:40:26 -0300

    Create matriz_ex19.c

---
+++ ProjetosAvaliativos/matriz_ex19.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#define L 10
+#define C 3
+
+int main(void) {
+  int i, j, num, matriz[L][C]; // coluna 0 = cara, coluna 1 = coroa, coluna 3 = desvio
+  int count = 1, desvioMaior = -999999999;
+  srand(time(NULL));
+
+  for(i = 0; i < L; i++){ 
+    for(j = 0; j < C; j++){
+      matriz[i][j] = 0;
+    }
+  }
+
+  while (count <= 1000){
+    for (i = 0; i < L; i++ ){ 
+        num = rand() % 2;
+        if (num == 0){
+          matriz[i][0]++;
+        }
+        else  
+          matriz[i][1]++;
+    }
+    count++;
+  }
+
+  for(i = 0; i < L; i++){ 
+    matriz[i][2] = matriz[i][0] - 500;
+    if (matriz[i][2] < 0)
+      matriz[i][2] = matriz[i][2]*(-1);
+
+    if (matriz[i][2] > desvioMaior)
+      desvioMaior = matriz[i][2];
+  }
+
+  printf("\tCARA\tCOROA\tDESVIO\n");
+  for(i = 0; i < L; i++){ 
+    for(j = 0; j < C; j++){
+      printf("\t%i", matriz[i][j]);
+    }
+    printf("\n");
+  }
+
+  return 0;
+}
